c89544033ac2ef2cd18b540c84ae55cd2e90c422
Kamil Ziółkowski <ziolek666@gmail.com>
2015-05-19 16:31:13 +0200

    dodano easter egg "duzodobrze"

--- client/templates/now.js
+++ client/templates/now.js
@@ -1,4 +1,5 @@
 Meteor.subscribe('reports');
+Session.setDefault('counter', 0);
 
 function getReports() {
 
@@ -24,4 +25,22 @@ Template.now.helpers({
     return getReports().count() === 0;
   },
   reports: getReports
+});
+
+Template.now.helpers({
+  counter: function() {
+    return Session.get('counter');
+  },
+  duzo: function() {
+    if (Session.get('counter') > 6) {
+      return "Jest dużodobrze."
+    };
+  },
+});
+
+Template.now.events({
+  'click #egg': function(event) {
+      Session.set('counter', Session.get('counter') + 1);
+      //alert(Session.get('counter'))
+  },
 });
